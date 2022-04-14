cf1ab6a8a849686b8922722a30d2a2914ea99ca6
Stefan Harsan Farr <shfarr@gmail.com>
2022-04-14 19:13:46 +0300

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+SERVICE_NAME="my.service"
+
+# This section is only needed if the computer also runs a service
+# and the service is using Identity Plus issued service certificate
+# the command will renew the service's TLS Server Certificate
 echo -n "updating agent identity ... "
-RESULT_A=$( /opt/identityplus/identityplus -f /etc/home.stefan.idplus.zone/agent-id -d Default update-service )
+RESULT_A=$( /opt/identityplus/identityplus -f /etc/$SERVICE_NAME/agent-id -d Default update-service )
 echo $RESULT_A
 
+# This will renew the TLS Client Certificate in the sepcified directory.
+# Regardless if the identity represents a service agent or a user agent bot the command is the same
 echo -n "updating service identity ... "
-RESULT_S=$( /opt/identityplus/identityplus -f /etc/home.stefan.idplus.zone/agent-id -d Default update )
+RESULT_S=$( /opt/identityplus/identityplus -f /etc/$SERVICE_NAME/agent-id -d Default update )
 echo $RESULT_S
 
+# Adjust acordingly if you removed the server section
 if [[ $RESULT_A == "renewed"  ||  $RESULT_S == "renewed" ]]
     then
 	echo "reloading nginx service ... "
